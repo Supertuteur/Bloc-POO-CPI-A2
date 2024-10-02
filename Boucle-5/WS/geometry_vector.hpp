5950545a129c82d579bc5b67ab468bb28b116647
Supertuteur <jfdollinger@cesi.fr>
2024-10-02 16:33:29 +0200

    Adding files for WS5

---
+++ Boucle-5/WS/geometry_vector.hpp
@@ -0,0 +1,21 @@
+#pragma once
+
+#include <math.h>
+#include "coordinate.hpp"
+
+// To be completed.
+
+class Vector {
+public:
+    float x, y;
+    Vector(float x, float y) : x(x), y(y) {}
+	
+	float getMagnitude() {
+		// To be completed.
+	}
+	
+	Coordinate getPosition() {
+		// To be completed.
+	}
+};
+
